a7dbefc638301adcbba65b0c1b22644178d88d50
arbitter <55300423+arbitter@users.noreply.github.com>
2020-10-17 15:00:53 -0500

    changed/removed most of script.js since most of it is handled elsewhere.

--- js/script.js
+++ js/script.js
@@ -1,44 +1,8 @@
-// 4 buttons for navigation at the top of each page. When clicked, they take the user to a different page on the website.
-let homeButton = document.getElementById("homeButton");
-let aboutMeButton = document.getElementById("aboutButton");
-let softwareButton = document.getElementById("softwareButton");
-let gameButton = document.getElementById("gameButton");
+//Navigation id used for the addNavigation function that
+//applies the navigation panel at the top of each page.
+let navigation = document.getElementById("navigation");
 
-//Learn More Link
-let learnMore = document.getElementById("learn-more");
-
-//contact me link at the bottom right of the page.
-let contactMeOverlay = document.getElementById("contact-me-overlay");
-
-//5 urls for each webpage. When clicking one of the navigation buttons go to these urls.
-let homeUrl = "./index.html";
-let aboutMeUrl = "./aboutme.html";
-let softwareUrl = "./softwareprojects.html";
-let gameUrl = "./gameprojects.html";
-let contactUrl = "./contact-me.html";
-
-//email link.
-let email = document.getElementById("email");
-let emailUrl = "mailto: averyclariday@my.unt.edu";
-
-
-//On click events for elements on every page of the website.
-homeButton.onclick = () => {
-	location.href = homeUrl;
+function addNavigation() {
+	navigation.innerHTML = "<div class=\"nav-grid-container\"><a class=\"nav-grid-item\" href=\"./index.html\">Home</a><a class=\"nav-grid-item\" href=\"./aboutme.html\">Aboue Me</a><a class=\"nav-grid-item\" href=\"./gameprojects.html\">Game Projects</a><a class=\"nav-grid-item\" href=\"./softwareprojects.html\">Software Projects</a></div>";
 }
-aboutMeButton.onclick = () => {
-	location.href = aboutMeUrl;
-}
-softwareButton.onclick = () => {
-	location.href = softwareUrl;
-}
-gameButton.onclick = () => {
-	location.href = gameUrl;
-}
-
-//On click events for elements only on certain pages of the website.
-if(learnMore != null){
-	learnMore.onclick = () => {
-		location.href = aboutMeUrl;
-	}
-}
+addNavigation();
