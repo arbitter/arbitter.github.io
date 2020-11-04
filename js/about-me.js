ca13527da2f4c044ed984dc1ab653073b8878c93
Avery C <averyclariday@my.unt.edu>
2020-11-04 14:00:12 -0600

    Improved code readability.

--- js/about-me.js
+++ js/about-me.js
@@ -49,37 +49,79 @@ Grid-Item Mouse Over Functions
 The functions used for each Grid-Items' mouse over event
 */
 function feMouseOver() {
-  frontEnd.innerHTML = "<dl><dt><h4>JavaScript</h4></dt><dt><h4>HTML</h4></dt><dt><h4>CSS</h4></dt><dt><h4>React</h4></dt><dt><h4>Angular</h4></dt></dl>";
+  frontEnd.innerHTML = "<dl>" +
+                          "<dt><h4>JavaScript</h4></dt>" +
+                          "<dt><h4>HTML</h4></dt>" +
+                          "<dt><h4>CSS</h4></dt>" +
+                          "<dt><h4>React</h4></dt>" +
+                          "<dt><h4>Angular</h4></dt>" +
+                        "</dl>";
 };
 
 function beMouseOver() {
-  backEnd.innerHTML = "<dl><dt><h4>C++</h4></dt><dt><h4>Java</h4></dt><dt><h4>Python</h4></dt><dt><h4>Spring</h4></dt><dt><h4>Django</h4></dt><dt><h4>Flask</h4></dt></dl>";
+  backEnd.innerHTML = "<dl>" +
+                        "<dt><h4>C++</h4></dt>" +
+                        "<dt><h4>Java</h4></dt>" +
+                        "<dt><h4>Python</h4></dt>" +
+                        "<dt><h4>Spring</h4></dt>" +
+                        "<dt><h4>Django</h4></dt>" +
+                        "<dt><h4>Flask</h4></dt>" +
+                      "</dl>";
 };
 
 function gdMouseOver() {
-  gameDev.innerHTML = "<dl><dt><h4>UE4</h4></dt><dt><h4>UMG</h4></dt><dt><h4>Blueprint</h4></dt><dt><h4>Unity</h4></dt><dt><h4>Box2D</h4></dt></dl>";
+  gameDev.innerHTML = "<dl>" +
+                        "<dt><h4>UE4</h4></dt>" +
+                        "<dt><h4>UMG</h4></dt>" +
+                        "<dt><h4>Blueprint</h4></dt>" +
+                        "<dt><h4>Unity</h4></dt>" +
+                        "<dt><h4>Box2D</h4></dt>" +
+                      "</dl>";
 }
 
 function stMouseOver() {
-  softwareTest.innerHTML = "<dl><dt><h4>Unit Testing</h4></dt><dt><h4>Branch Testing</h4></dt><dt><h4>Testing Automation</h4></dt><dt><h4>Appium</h4></dt><dt><h4>Selenium</h4></dt></dl>";
+  softwareTest.innerHTML = "<dl>" +
+                              "<dt><h4>Unit Testing</h4></dt>" +
+                              "<dt><h4>Branch Testing</h4></dt>" +
+                              "<dt><h4>Testing Automation</h4></dt>" +
+                              "<dt><h4>Appium</h4></dt>" +
+                              "<dt><h4>Selenium</h4></dt>" +
+                            "</dl>";
 }
 
 function uxMouseOver() {
-  userEx.innerHTML = "<dl><dt><h4>User Stories</h4></dt><dt><h4>Use Cases</h4></dt><dt><h4>Wireframing</h4></dt><dt><h4>Information Architecture</h4></dt></dl>";
+  userEx.innerHTML = "<dl>" +
+                        "<dt><h4>User Stories</h4></dt>" +
+                        "<dt><h4>Use Cases</h4></dt>" +
+                        "<dt><h4>Wireframing</h4></dt>" +
+                        "<dt><h4>Information Architecture</h4></dt>" +
+                      "</dl>";
 }
 
 function ssMouseOver() {
-  softSkills.innerHTML = "<dl><dt><h4>Communication</h4></dt><dt><h4>Collaboration</h4></dt><dt><h4>Critical Thinking</h4></dt><dt><h4>Work Ethic</h4></dt></dl>";
+  softSkills.innerHTML = "<dl>" +
+                            "<dt><h4>Communication</h4></dt>" +
+                            "<dt><h4>Collaboration</h4></dt>" +
+                            "<dt><h4>Critical Thinking</h4></dt>" +
+                            "<dt><h4>Work Ethic</h4></dt>" +
+                          "</dl>";
 }
 
 function dtMouseOver(){
   console.log("Dev Tools Mouse Over Logged");
-  devTools.innerHTML = "<dl><dt><h4>GitHub</h4></dt><dt><h4>Subversion</h4></dt><dt><h4>Trello</h4></dt></dl>";
+  devTools.innerHTML = "<dl>" +
+                          "<dt><h4>GitHub</h4></dt>" +
+                          "<dt><h4>Subversion</h4></dt>" +
+                          "<dt><h4>Trello</h4></dt>" +
+                        "</dl>";
 }
 
 function methodsMouseOver() {
   console.log("Methods Mouse Over Logged");
-  methods.innerHTML = "<dl><dt><h4>Agile</h4></dt><dt><h4>Waterfall</h4></dt></dl>";
+  methods.innerHTML = "<dl>" +
+                        "<dt><h4>Agile</h4></dt>" +
+                        "<dt><h4>Waterfall</h4></dt>" +
+                      "</dl>";
 }
 /// End Mouse Over Functions
 
